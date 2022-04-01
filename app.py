c9dc46ef088e1113d9a86e8470684126f96673f3
KatyaSoboleva68 <KatyaSobolewaTmbw@mail.ru>
2022-04-01 17:03:11 +0300

    6

--- app.py
+++ app.py
@@ -40,8 +40,18 @@ def template_handler(req, resp):
         "index.html",
         context={"name": "Bumbo", "title": "Best Framework"}
     ).encode()
-        
+
+
+@app.route("/exception")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be used.")
+
 def handler(req, resp):
     resp.text = "sample"
 
 app.add_route("/sample", handler)
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+app.add_exception_handler(custom_exception_handler)
