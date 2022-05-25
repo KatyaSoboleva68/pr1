69c0716280bd1c1e2ca2dcc0c9eeaac7fd5d8a5d
KatyaSoboleva68 <KatyaSobolewaTmbw@mail.ru>
2022-05-25 15:03:33 +0300

    9

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ def sum(request, response, num_1, num_2):
     total = int(num_1) + int(num_2)
     response.text = f"{num_1} + {num_2} = {total}"
 
+@app.route("/exception")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be used.")
+
+@app.route("/template")
+def template_handler(req, resp):
+    resp.html = app.template("index.html", context={"name": "Bumbo", "title": "Best Framework"})
+
+@app.route("/json")
+def json_handler(req, resp):
+    resp.json = {"name": "data", "type": "JSON"}
+
+@app.route("/text")
+def text_handler(req, resp):
+    resp.text = "This is a simple text"
+
 @app.route("/book")
 class BooksResource:
     def get(self, req, resp):
@@ -33,18 +49,6 @@ class BooksResource:
 
     def post(self, req, resp):
         resp.text = "Endpoint to create a book"
-
-@app.route("/template")
-def template_handler(req, resp):
-    resp.body = app.template(
-        "index.html",
-        context={"name": "Bumbo", "title": "Best Framework"}
-    ).encode()
-
-
-@app.route("/exception")
-def exception_throwing_handler(request, response):
-    raise AssertionError("This handler should not be used.")
 
 def handler(req, resp):
     resp.text = "sample"
