9f155c0729cbfd6c3d7b75fb820a0496e062036c
KatyaSoboleva68 <KatyaSobolewaTmbw@mail.ru>
2022-05-25 15:02:11 +0300

    7

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-import inspect
 import os
+import inspect
 
 from parse import parse
 from webob import Request, Response
@@ -8,9 +8,10 @@ from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
 from jinja2 import Environment, FileSystemLoader
 from whitenoise import WhiteNoise
 
+from middleware import Middleware
+
 
 class API:
-
     def __init__(self, templates_dir="templates", static_dir="static"):
         self.routes = {}
 
@@ -22,8 +23,16 @@ class API:
 
         self.whitenoise = WhiteNoise(self.wsgi_app, root=static_dir)
 
+        self.middleware = Middleware(self)
+
     def __call__(self, environ, start_response):
-        return self.whitenoise(environ, start_response)
+        path_info = environ["PATH_INFO"]
+
+        if path_info.startswith("/static"):
+            environ["PATH_INFO"] = path_info[len("/static"):]
+            return self.whitenoise(environ, start_response)
+
+        return self.middleware(environ, start_response)
 
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
@@ -61,38 +70,6 @@ class API:
 
         handler, kwargs = self.find_handler(request_path=request.path)
 
-        if handler is not None:
-            if inspect.isclass(handler):
-                handler = getattr(handler(), request.method.lower(), None)
-                if handler is None:
-                    raise AttributeError("Method not allowed", request.method)
-
-            handler(request, response, **kwargs)
-        else:
-            self.default_response(response)
-
-        return response
-
-    def test_session(self, base_url="http://testserver"):
-        session = RequestsSession()
-        session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
-        return session
-
-        
-    def template(self, template_name, context=None):
-        if context is None:
-            context = {}
-
-        return self.templates_env.get_template(template_name).render(**context)
-
-    def add_exception_handler(self, exception_handler):
-        self.exception_handler = exception_handler
-
-    def handle_request(self, request):
-        response = Response()
-
-        handler, kwargs = self.find_handler(request_path=request.path)
-
         try:
             if handler is not None:
                 if inspect.isclass(handler):
@@ -108,4 +85,23 @@ class API:
                 raise e
             else:
                 self.exception_handler(request, response, e)
+
         return response
+
+    def test_session(self, base_url="http://testserver"):
+        session = RequestsSession()
+        session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
+        return session
+
+
+    def template(self, template_name, context=None):
+        if context is None:
+            context = {}
+
+        return self.templates_env.get_template(template_name).render(**context)
+
+    def add_exception_handler(self, exception_handler):
+        self.exception_handler = exception_handler
+
+    def add_middleware(self, middleware_cls):
+        self.middleware.add(middleware_cls)
